a9f350032d16625ed445a88bd1e23a57d2033d8f
Mariah Molenaer <mariah.molenaer1@marist.edu>
2016-02-25 18:55:31 -0500

    Added a personal welcome message using player's name

--- GameEngine.hs
+++ GameEngine.hs
@@ -1,18 +1,36 @@
--- GameEngine.hs
+-- GameEngine.hs - NATURE'S PANTRY Game Engine
 -- Author:  Mariah Molenaer
 
 import System.IO
 
+
 data Player = Player {
     playerName :: String
-} deriving Show
+} 
 
+-- shows player name without quotation marks
+instance Show Player where
+    show (Player n) = id n
+
+-- prompts user to enter a name
 getPlayerName :: IO Player
 getPlayerName = do
     hPutStr stderr "Please enter your name: "
-    playerName <- fmap read getLine
+    playerName <- getLine
     return (Player playerName)
 
-main = do
-    getPlayerName
 
+main = do
+    header
+    n <- getPlayerName
+    welcomeMsg n
+
+
+-- introductory message signifying the game has begun
+header :: IO ()
+header = putStrLn $ "\n    NATURE'S PANTRY Text Adventure Game    "
+                 ++ "\n===========================================\n"
+
+-- personal welcome message using the player's name
+welcomeMsg :: Player -> IO ()
+welcomeMsg n = putStrLn $ "\nWelcome to NATURE’S PANTRY, " ++ (show n) ++ ", your favorite alternative grocery store!\n"
